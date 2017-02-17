854f7e41636968843f3aafe2d0a899fa0dfcb8ab
rghossi <rafael.ghossi@gmail.com>
2017-02-17 14:46:29 -0200

    integrating websockets and redux store on server side

--- server/app.js
+++ server/app.js
@@ -11,6 +11,14 @@ const io = socketIO(server);
 const PORT = process.env.PORT || 3000;
 
 const store = createStore(reducer);
+store.subscribe(
+  () => io.emit('state', store.getState().toJS())
+);
+
+io.on('connection', (socket) => {
+  socket.emit('state', store.getState().toJS());
+  socket.on('action', store.dispatch.bind(store));
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
