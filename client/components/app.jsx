5f578ecc9ef4f8b352204d11e72dff3032726a10
rghossi <rafael.ghossi@gmail.com>
2017-02-20 13:51:39 -0300

    preparing client side to receive data from server

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -5,16 +5,104 @@ import MyNavbar from './navbar';
 import MyFooter from './footer';
 import MyChart from './myChart';
 import NewStockForm from './newStockForm';
-import { Grid } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
+import fetch from 'isomorphic-fetch';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			datasets: [
+        {
+          label: 'My First dataset',
+          fill: false,
+          lineTension: 0,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [{
+            x: new Date(2016, 10, 10),
+            y: 10
+          },{
+            x: new Date(2016, 11, 10),
+            y: 15
+          },{
+            x: new Date(2016, 12, 10),
+            y: 20
+          },{
+            x: new Date(2017, 1, 10),
+            y: 15
+          },{
+            x: new Date(2017, 2, 10),
+            y: 25
+          }]
+        }
+      ]
+		}
+	}
+
+	getStockmarkets() {
+		const {stockMarkets} = this.props;
+		fetch("/api/getStockmarkets", {credentials: 'same-origin'})
+      .then(response => response.json())
+      .then(json => {
+			  if (err) throw err;
+			  let newDatasets = this.state.datasets.slice(0);
+			  let data = json.map((q) => {
+			  	return {
+	          x: new Date(q.date),
+	          y: q.open
+	        }
+			  })
+			  newDatasets.push({
+	        label: json[0].symbol,
+	        fill: false,
+	        lineTension: 0,
+	        backgroundColor: 'rgba(75,192,192,0.4)',
+	        borderColor: 'rgba(75,192,192,1)',
+	        borderCapStyle: 'butt',
+	        borderDash: [],
+	        borderDashOffset: 0.0,
+	        borderJoinStyle: 'miter',
+	        pointBorderColor: 'rgba(75,192,192,1)',
+	        pointBackgroundColor: '#fff',
+	        pointBorderWidth: 1,
+	        pointHoverRadius: 5,
+	        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+	        pointHoverBorderColor: 'rgba(220,220,220,1)',
+	        pointHoverBorderWidth: 2,
+	        pointRadius: 1,
+	        pointHitRadius: 10,
+	        data: data
+	      })
+			})
+	}
+
+	componentDidMount() {
+		// this.getStockmarkets();
+	}
+
 	render() {
 		return (
 			<div>
 				<MyNavbar />
 				<Grid>
-					<MyChart />
 					<NewStockForm />
+					<Row>
+						<MyChart datasets={this.state.datasets}/>
+					</Row>
 				</Grid>
 				<MyFooter />
 			</div>
--- client/components/myChart.jsx
+++ client/components/myChart.jsx
@@ -4,47 +4,11 @@ import {Line} from 'react-chartjs-2';
 export default class MyChart extends Component {
   render() {
     const data = {
-      datasets: [
-        {
-          label: 'My First dataset',
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [{
-            x: new Date(2016, 10, 10),
-            y: 10
-          },{
-            x: new Date(2016, 11, 10),
-            y: 15
-          },{
-            x: new Date(2016, 12, 10),
-            y: 20
-          },{
-            x: new Date(2017, 1, 10),
-            y: 15
-          },{
-            x: new Date(2017, 2, 10),
-            y: 25
-          }]
-        }
-      ]
+      datasets: this.props.datasets
     };
 
     const options = {
+      responsive: true,
       scales: {
         xAxes: [{
           type: 'time',
@@ -57,7 +21,7 @@ export default class MyChart extends Component {
 
     return (
       <div>
-        <Line data={data} options={options}/>
+        <Line data={data} options={options} height={100} />
       </div>
     );
   }
