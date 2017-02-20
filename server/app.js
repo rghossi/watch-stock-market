d7262b1606a61079e559f2e0ccf35922560cb590
rghossi <rafael.ghossi@gmail.com>
2017-02-20 14:23:44 -0300

    integrated backend implementation of chart data with client

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import {AppContainer} from '../client/components/app';
 import configureStore from '../client/configureStore';
+import yahooFinance from 'yahoo-finance';
 
 const app = express();
 const server = http.createServer(app);
@@ -28,6 +29,55 @@ io.on('connection', (socket) => {
 app.use(express.static('dist'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.get('/api/stockmarkets', function(req, res) {
+	let stocks = store.getState().stockMarkets;
+	if (stocks.length < 1) {
+		res.json({datasets: []});
+		return;
+	}
+	yahooFinance.historical({
+	  symbols: stocks,
+	  from: '2016-01-01',
+	  to: new Date().toISOString().substring(0, 10),
+	  period: 'd'
+	}, function (err, result) {
+		if (err) throw err;
+		let datasets = [];
+		stocks.forEach(function(stock){
+			let quotes = result[stock];
+			let data = quotes.map((q) => {
+		  	return {
+	        x: new Date(q.date),
+	        y: q.open
+	      }
+		  })
+		  const dataset = {
+	      label: stock,
+	      fill: false,
+	      lineTension: 0,
+	      backgroundColor: 'rgba(75,192,192,0.4)',
+	      borderColor: 'rgba(75,192,192,1)',
+	      borderCapStyle: 'butt',
+	      borderDash: [],
+	      borderDashOffset: 0.0,
+	      borderJoinStyle: 'miter',
+	      pointBorderColor: 'rgba(75,192,192,1)',
+	      pointBackgroundColor: '#fff',
+	      pointBorderWidth: 1,
+	      pointHoverRadius: 5,
+	      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+	      pointHoverBorderColor: 'rgba(220,220,220,1)',
+	      pointHoverBorderWidth: 2,
+	      pointRadius: 1,
+	      pointHitRadius: 10,
+	      data: data
+	    }
+	    datasets.push(dataset);
+		})
+		res.json({datasets: datasets});
+	});
+});
 
 app.get('/', function (req, res) {
   const clientStore = configureStore();
